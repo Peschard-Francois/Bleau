d6fa72f8be94690375152749c2216cb1d89c7bba
FrancoisPG <francois@lifeaz.fr>
2023-05-15 11:55:09 +0200

    Install Api Platform and Create first Way for Route Entity

--- src/Entity/Route.php
+++ src/Entity/Route.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\RouteRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: RouteRepository::class)]
+#[ApiResource]
 class Route
 {
     #[ORM\Id]
